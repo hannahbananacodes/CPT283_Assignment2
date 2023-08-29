5e954300c6605f0c3666c0c0530d3c4960a8b790
Hannah Holmes <hjholmes@hawaii.edu>
2023-08-29 15:13:01 +0200

    edited to set up for form handling

---
+++ checkform.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Weather Wizards Registration Submission Receipt</title>
+</head>
+<body>
+<?php 
+
+# checkform.php
+# Created 29 Aug 2023
+# Created by Hannah Holmes
+# This script handles the form created for my CPT 283 PHP class assignment 2
+
+
+?>
+</body>
+</html>
